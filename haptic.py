5f442ab3ba414f60fe53b9e2768e1539cb9a97d5
bharadwaj1098 <bharadwaj.reddy98@gmail.com>
2021-06-28 14:31:37 -0400

    haptic code added

---
+++ haptic.py
@@ -0,0 +1,12 @@
+import numpy as np
+from scipy.integrate import odeint
+import pandas as pd
+import random
+from random import randint 
+from sympy import *
+from spring_damp import mass_damp_spring 
+import matplotlib.pyplot as plt
+random.seed(49)
+
+class haptic(mass_damp_spring):
+    pass 
